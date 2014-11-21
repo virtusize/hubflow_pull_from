56407523cebfb5f380e4c737053768729771fd54
Igor Khomyakov <igor@virtusize.com>
2014-11-21 14:03:39 +0300

    Refactor: code cleanups, refactoring

--- pull_from.py
+++ pull_from.py
@@ -28,7 +28,11 @@ import re
 # http_client.HTTPConnection.debuglevel = 1
 
 
-def latest_commit(url, headers):
+def latest_commit(api_url, branch, headers, since=0):
+    if since:
+        url = '{}/commits?sha={}&since={}'.format(api_url, branch, since)
+    else:
+        url = '{}/commits?sha={}'.format(api_url, branch)
     r = requests.get(url, headers=headers)
     if (r.ok):
         commits = r.json()
@@ -49,6 +53,14 @@ def commit_timestamp(url, headers):
         return d.timestamp
 
 
+def repo_normalization(name):
+    if name:
+        regex = r'(?:git@|https?:/+)?(?:github\.com)?[:/]?(.*?)(?:.git)?$'
+        m = re.search(regex, name)
+        if m:
+            return m.group(1)
+
+
 def main():
     arguments = docopt(__doc__, version='0.0.1')
     api_token = arguments.get('--token', None)
@@ -58,62 +70,56 @@ def main():
     headers = {'Authorization': 'token %s' % api_token}
 
     repo = arguments.get('--repo', None)
+    repo = repo_normalization(repo)
+
     if not repo:
-        print "Invalid repository"
-        return 1
-    m = re.search('(?:git@|https?:/+)?(?:github\.com)?[:/]?(.*?)(?:.git)?$', repo)
-    if m:
-        repo = m.group(1)
-    else:
         print "Invalid repository"
         return 1
 
     api_url = 'https://api.github.com/repos/%s' % repo
 
     pull_from = {}
-    r = requests.get(api_url, headers=headers)
-    week = arrow.utcnow().replace(weeks=-1)
-    # timestamp should be in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
-    since = week.format('YYYY-MM-DDTHH:mm:ss') + 'Z'
-    commit_obj = latest_commit('%s/commits?sha=%s&since=%s'
-                               % (api_url, 'master', since), headers=headers)
-    master_date = arrow.get(commit_obj['commit']['committer']['date'])
-#    print "MASTER DATE ", master_date
-    pull_from['branch'] = 'master'
-    pull_from['date'] = master_date
 
+    r = requests.get(api_url, headers=headers)
     if (r.ok):
         r = requests.get(api_url + '/branches', headers=headers)
         if (r.ok):
             branches = r.json()
             print "Branches: ", ", ".join([b['name'] for b in branches])
+            # there are many commits in a 'master' branch
+            week = arrow.utcnow().replace(weeks=-1)
+            # timestamp should be in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
+            since = week.format('YYYY-MM-DDTHH:mm:ss') + 'Z'
+            commit_obj = latest_commit(api_url, 'master', since=since,
+                                        headers=headers)
+            master_date = arrow.get(commit_obj['commit']['committer']['date'])
+            # print "MASTER DATE ", master_date
+            pull_from['branch'] = 'master'
+            pull_from['date'] = master_date
 
             releases = [b for b in branches if 'release/' in b['name']]
-            hotfixes = [b for b in branches if 'hotfix/' in b['name']]
+            hotfixes = [b for b in branches if 'hotfix/'  in b['name']]
 
             if releases:
                 print "Releases: ", ", ".join([b['name'] for b in releases])
-                rel = sorted(releases, key=lambda k: k['name'])[-1]
-                commit_obj = latest_commit('%s/commits?sha%s'
-                                           % (api_url, rel['name']),
-                                           headers=headers)
-                rel_date = arrow.get(commit_obj['commit']['committer']['date'])
-#                print "REL DATE", rel_date
-                if (rel_date - pull_from['date']).seconds > 0:
-                    pull_from['branch'] = rel['name']
-                    pull_from['date'] = rel_date
+                b = sorted(releases, key=lambda k: k['name'])[-1]
+                commit_obj = latest_commit(api_url, b['name'], headers=headers)
+                b_date = arrow.get(commit_obj['commit']['committer']['date'])
+                # print "REL DATE", b_date
+                if (b_date - pull_from['date']).seconds > 0:
+                    pull_from['branch'] = b['name']
+                    pull_from['date'] = b_date
 
             if hotfixes:
                 print "Hotfixes: ", ", ".join([b['name'] for b in hotfixes])
-                fix = sorted(hotfixes, key=lambda k: k['name'])[-1]
-                commit_obj = latest_commit('%s/commits?sha%s'
-                                           % (api_url, fix['name']),
-                                           headers=headers)
-                fix_date = arrow.get(commit_obj['commit']['committer']['date'])
-#                print "FIX DATE", fix_date
-                if (fix_date - pull_from['date']).seconds > 0:
-                    pull_from['branch'] = fix['name']
-                    pull_from['date'] = fix_date
+                b = sorted(hotfixes, key=lambda k: k['name'])[-1]
+                commit_obj = latest_commit(api_url, b['name'], headers=headers)
+                b_date = arrow.get(commit_obj['commit']['committer']['date'])
+                #  print "FIX DATE", b_date
+                if (branch_date - pull_from['date']).seconds > 0:
+                    pull_from['branch'] = b['name']
+                    pull_from['date'] = b_date
+
             print "Pull from ", pull_from['branch']
 
         else:
